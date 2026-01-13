059f2f46a68874e1ac0e59313f4525112611901f
nenugdi <siguodng@sina.com>
2026-01-13 10:02:09 +0800

    第二次提交
    第二次提交

--- Gui/pause_menu/inventory/scripts/inventory_slot_ui.gd
+++ Gui/pause_menu/inventory/scripts/inventory_slot_ui.gd
@@ -38,6 +38,7 @@ func _on_pressed() -> void:
 				return
 			slot_data.quantity -= 1
 			label.text = str(slot_data.quantity)
-			
-			
+			if slot_data.quantity>0: #onpress不会触发pausemenu的刷新，所有使用道具后仍然维持焦点。
+				self.grab_focus()
+				
 	pass # Replace with function body.
