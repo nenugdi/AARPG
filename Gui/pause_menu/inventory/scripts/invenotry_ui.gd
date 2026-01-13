33eaf8c7806b1bd0e34b4771a127b0cc1783fcbc
nenugdi <siguodng@sina.com>
2026-01-13 09:36:58 +0800

    1
    第一次测试提交

--- Gui/pause_menu/inventory/scripts/invenotry_ui.gd
+++ Gui/pause_menu/inventory/scripts/invenotry_ui.gd
@@ -3,7 +3,7 @@ class_name InventoryUI extends Control #GridContainer对应脚本
 const INVENTORY_SLOT = preload("uid://bmhdevn0urorx") #InventorySlot场景，也就是道具按钮
 
 @export var data : InventoryData #InventoryData默认数组SlotData大小为10
-
+var foucs_index : int
 func _ready() -> void:
 	PauseMenu.shown.connect(update_inventory) #PauseMenu是全局类
 	PauseMenu.hidden.connect(clear_inventory)
@@ -22,10 +22,18 @@ func update_inventory()->void:
 		var new_slot = INVENTORY_SLOT.instantiate()
 		add_child(new_slot)
 		new_slot.slot_data = s
-		
+		new_slot.focus_entered.connect( item_focused )
 	get_child(0).grab_focus()
 
 
 func on_inventory_changed()->void:
 	clear_inventory()
 	update_inventory()
+	get_child(foucs_index).grab_focus()
+	
+func item_focused()->void:
+	for i in get_child_count():
+		if get_child(i).has_focus():
+			foucs_index = i
+			return
+	pass
